6000ce8f04ddeaf7460398475b764bff7d61fe01
JavaDog <jbs6911@psu.edu>
2023-08-31 22:27:43 -0400

    added 3 dogs

--- Dog.java
+++ Dog.java
@@ -1,3 +1,5 @@
+// Enumeration for Breed
+enum Breed { POODLE, WOLF, CORGEY }
 public class Dog {
 
     private String name;
@@ -5,11 +7,14 @@ public class Dog {
     private String color;
     private double weight;
 
-    public Dog(String name, int age, String color, double weight) {
+    private Breed breedType;
+
+    public Dog(String name, int age, String color, double weight, Breed breedType) {
         this.name = name;
         this.age = age;
         this.color = color;
         this.weight = weight;
+        this.breedType = breedType;
     }
 
     public void bark() {
@@ -29,6 +34,12 @@ public class Dog {
     }
     public void setName(String name) {
         this.name = name;
+    }
+    public Breed getBreedType() {
+        return breedType;
+    }
+    public void setBreedType(Breed breedType) {
+        this.breedType = breedType;
     }
     public int getAge() {
         return age;
@@ -53,7 +64,7 @@ public class Dog {
     // Main method
     public static void main(String[] args) {
 //dog 1
-        Dog myDog1 = new Dog("Snoopy", 12, "brown", 20.6);
+        Dog myDog1 = new Dog("Snoopy", 12, "brown", 20.6, Breed.CORGEY);
         myDog1.bark();
 
 
@@ -61,21 +72,24 @@ public class Dog {
         System.out.println("Age: " + myDog1.getAge());
         System.out.println("Color: " + myDog1.getColor());
         System.out.println("Weight: " + myDog1.getWeight() + " lbs.");
+        System.out.println("Breed: " + myDog1.getBreedType());
 //dog 2
-        Dog myDog2 = new Dog("Spot", 5, "Black", 13.56);
+        Dog myDog2 = new Dog("Spot", 5, "Black", 13.56,Breed.POODLE);
         myDog2.sleep();
 
         System.out.println("Name: " + myDog2.getName());
         System.out.println("Age: " + myDog2.getAge());
         System.out.println("Color: " + myDog2.getColor());
         System.out.println("Weight: " + myDog2.getWeight() + " lbs.");
+        System.out.println("Breed: " + myDog2.getBreedType());
 //dog 3
-        Dog myDog3 = new Dog("Wolf", 7, "White", 35.84);
+        Dog myDog3 = new Dog("Wolf", 7, "White", 35.84, Breed.WOLF);
         myDog3.eat();
 
         System.out.println("Name: " + myDog3.getName());
         System.out.println("Age: " + myDog3.getAge());
         System.out.println("Color: " + myDog3.getColor());
         System.out.println("Weight: " + myDog3.getWeight() + " lbs.");
+        System.out.println("Breed: " + myDog3.getBreedType());
     }
 }
