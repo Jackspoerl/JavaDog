b9da783812810d3fbedd51f1d418e82567b33c01
JavaDog <jbs6911@psu.edu>
2023-08-31 20:59:10 -0400

    added 3 dogs

--- Dog.java
+++ Dog.java
@@ -3,7 +3,6 @@ public class Dog {
     private String name;
     private int age;
     private String color;
-    private String hairType;
     private double weight;
 
     public Dog(String name, int age, String color, double weight) {
@@ -14,7 +13,7 @@ public class Dog {
     }
 
     public void bark() {
-        System.out.println(name + " is barking");
+        System.out.println(name + " is barking.");
     }
 
     public void sleep() {
@@ -53,15 +52,30 @@ public class Dog {
 
     // Main method
     public static void main(String[] args) {
-        Dog myDog1 = new Dog("Coco", 12, "brown", 20.6);
+//dog 1
+        Dog myDog1 = new Dog("Snoopy", 12, "brown", 20.6);
         myDog1.bark();
-        myDog1.sleep();
-        myDog1.eat();
+
 
         System.out.println("Name: " + myDog1.getName());
         System.out.println("Age: " + myDog1.getAge());
         System.out.println("Color: " + myDog1.getColor());
         System.out.println("Weight: " + myDog1.getWeight() + " lbs.");
+//dog 2
+        Dog myDog2 = new Dog("Spot", 5, "Black", 13.56);
+        myDog2.sleep();
 
+        System.out.println("Name: " + myDog2.getName());
+        System.out.println("Age: " + myDog2.getAge());
+        System.out.println("Color: " + myDog2.getColor());
+        System.out.println("Weight: " + myDog2.getWeight() + " lbs.");
+//dog 3
+        Dog myDog3 = new Dog("Wolf", 7, "White", 35.84);
+        myDog3.eat();
+
+        System.out.println("Name: " + myDog3.getName());
+        System.out.println("Age: " + myDog3.getAge());
+        System.out.println("Color: " + myDog3.getColor());
+        System.out.println("Weight: " + myDog3.getWeight() + " lbs.");
     }
 }
